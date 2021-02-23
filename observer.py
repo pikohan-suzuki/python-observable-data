1683b6d208ee16feecee43d8b7012909dd753078
pikohan-suzuki <b1702996@planet.kanazawa-it.ac.jp>
2021-02-23 16:23:03 +0900

    [fix] fix argument type of on_changed function

--- observer.py
+++ observer.py
@@ -6,5 +6,5 @@ class Observer(Generic[T]):
     def __init__(self,func:Callable[[T],None]):
         self.on_changed_func = func
 
-    def on_changed(self,value):
+    def on_changed(self,value:T):
         self.on_changed_func(value)
