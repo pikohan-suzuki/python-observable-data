f60d2dc8e7b75395957f1d0354783f783dd24358
pikohan-suzuki <b1702996@planet.kanazawa-it.ac.jp>
2021-02-23 14:49:47 +0900

    [add] observer class

---
+++ observer.py
@@ -0,0 +1,10 @@
+from typing import TypeVar,Callable,Generic
+
+T = TypeVar('T')
+
+class Observer(Generic[T]):
+    def __init__(self,func:Callable[[T],None]):
+        self.on_changed_func = func
+
+    def on_changed(self,value):
+        self.on_changed_func(value)
