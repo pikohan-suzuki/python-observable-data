e88d610bab7f8091507e5c30a9cc9f06f3d4b50f
pikohan-suzuki <b1702996@planet.kanazawa-it.ac.jp>
2021-02-23 14:49:28 +0900

    [add] observable-data class

---
+++ observable_data.py
@@ -0,0 +1,27 @@
+from typing import TypeVar,Callable,Generic,List
+from observer import Observer
+
+T = TypeVar('T')
+class ObservableData(Generic[T]):
+    def __init__(self,data:T) -> None:
+        self.data = data
+        self.observers:List[Observer] = []
+    
+    @property
+    def value(self) -> T:
+        print("value_getter")
+        return self.data
+
+    @value.setter
+    def value(self,value:T):
+        print("value_setter")
+        self.data = value
+        self.notificate()
+
+    def observe(self,observer:Observer):
+        self.observers += [observer]
+
+    def notificate(self):
+        for observer in self.observers:
+            observer.on_changed(self.data)
+
